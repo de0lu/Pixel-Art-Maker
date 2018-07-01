099fd59f98c1858a836921aa8541ed8e3e5c61c3
de0lu <36403555+de0lu@users.noreply.github.com>
2018-07-01 01:20:32 +0100

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,32 @@
+// Select color input
+// Select size input
+var height, width, color;
+// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    const height = $('#inputHeight').val();
+    const width = $('#inputWidth').val();
+    makeGrid(height, width);
+})
+
+function makeGrid(row, col) {
+
+  // Your code goes here!
+$('#pixelCanvas').find('tr').remove();
+  for (var i = 1; i <= row; i++) {
+    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+      for (var j = 1; j <= col; j++) {
+        $('#table' + i).append('<td></td>');
+    }
+}
+    // add colors to the cells
+$('td').click(function addColor(){
+  color = $('#colorPicker').val();
+    if ($(this).attr('style')) {
+            $(this).removeAttr('style')
+        } else {
+            $(this).attr('style', 'background-color:' + color);
+        }
+    })
+
+}
