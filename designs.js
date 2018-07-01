f2cad0091a471a09379ce6ba84910f017ebbfa40
de0lu <36403555+de0lu@users.noreply.github.com>
2018-07-01 21:11:22 +0100

    Update designs.js
    Added a download art function. Yay!

--- designs.js
+++ designs.js
@@ -1,6 +1,11 @@
 // Select color input
 // Select size input
 let height, width, color;
+const pixelCanvas = $('#pixelCanvas');
+const savedImageBox = $('.saved-image-box');
+const saveButton = $('#save-button');
+let getCanvas = "";
+
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function (event) {
     event.preventDefault();
@@ -12,21 +17,35 @@ $('#sizePicker').submit(function (event) {
 function makeGrid(row, col) {
 
   // Your code goes here!
-$('#pixelCanvas').find('tr').remove();
+  $('#pixelCanvas').find('tr').remove();
   for (var i = 1; i <= row; i++) {
-    $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+      $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
       for (var j = 1; j <= col; j++) {
-        $('#table' + i).append('<td></td>');
+          $('#table' + i).append('<td></td>');
+        }
     }
-}
     // add colors to the cells
-$('td').click(function addColor(){
-  color = $('#colorPicker').val();
-    if ($(this).attr('style')) {
+    $('td').click(function addColor(){
+        color = $('#colorPicker').val();
+
+        if ($(this).attr('style')) {
             $(this).removeAttr('style')
         } else {
             $(this).attr('style', 'background-color:' + color);
         }
     })
-
+}
+//save Pixel art
+function saveImage() {
+	html2canvas(pixelCanvas, {
+		onrendered: function (canvas) {
+			// Creates a new link for download
+			let imageURI = canvas.toDataURL();
+			let myLink = document.createElement('a');
+			myLink.download = "yourart.png";
+			myLink.href = imageURI;
+			document.body.appendChild(myLink);
+			myLink.click();
+		}
+	});
 }
